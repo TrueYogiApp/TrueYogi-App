c8a49cc54eea91b8a05f2f492bb997dbe9eff3c7
iCrownMan <trueyogiapp@gmail.com>
2025-09-25 06:48:47 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,10 @@
 self.addEventListener('fetch', event => {
   const url = event.request.url;
 
-  // Stale-while-revalidate for HTML - best of both worlds
+  // Always network-first for HTML
   if (event.request.mode === 'navigate') {
     event.respondWith(
-      caches.match(event.request).then(cached => {
-        // Always try to fetch fresh version
-        const fetchPromise = fetch(event.request)
-          .then(networkResponse => {
-            // Update cache with fresh version
-            caches.open('dynamic-cache').then(cache => {
-              cache.put(event.request, networkResponse.clone());
-            });
-            return networkResponse;
-          })
-          .catch(() => { /* Ignore fetch errors */ });
-
-        // Return cached version immediately if available, otherwise wait for network
-        return cached || fetchPromise;
-      })
+      fetch(event.request).catch(() => caches.match(event.request))
     );
     return;
   }
